4165ac500478f62517a764b63d54061d3089542c
Luca Di Bello <lucadibello@proton.me>
2024-10-31 18:51:25 +0000

    Updates script + testing report output

--- scripts/run_pdm.sh
+++ scripts/run_pdm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-PMD_HOME=/opt/pmd/bin
-PROJ_HOME=~/resilience4j
+PMD=/opt/pmd/bin/pmd
+PROJ_HOME=/workspaces/design-evaluation/resilience4j
 ANALYSIS_RULES=java-basic,java-design,java-unused
-REPORT_OUT=~/out/report.html
+REPORT_OUT=/workspaces/design-evaluation/out/report.html
 
-$PMD_HOME/bin/run.sh pmd \
+$PMD check \
   -d $PROJ_HOME \
   -f html \
-  -rulesets $ANALYSIS_RULES \
-  $REPORT_OUT
+  --rulesets $ANALYSIS_RULES \
+  --report-file $REPORT_OUT \
+  --debug
