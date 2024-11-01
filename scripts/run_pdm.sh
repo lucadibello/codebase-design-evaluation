2d3984a6a380e716cd4d45111335cedb2433e729
Luca Di Bello <lucadibello@proton.me>
2024-11-01 10:29:07 +0000

    Updated PDM command

--- scripts/run_pdm.sh
+++ scripts/run_pdm.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 PMD=/opt/pmd/bin/pmd
 PROJ_HOME=/workspaces/design-evaluation/resilience4j
-ANALYSIS_RULES=java-basic,java-design,java-unused
+ANALYSIS_RULES=rulesets/java/quickstart.xml
 REPORT_OUT=/workspaces/design-evaluation/out/report.html
+JAVA_VERSION=17
 
 $PMD check \
   -d $PROJ_HOME \
+  -R $ANALYSIS_RULES \
   -f html \
-  --rulesets $ANALYSIS_RULES \
-  --report-file $REPORT_OUT \
-  --debug
+  --use-version java-$JAVA_VERSION \
+  --report-file $REPORT_OUT
