ceb379d2adf60e6308982f5d5dc4f3b0650087c4
Luca Di Bello <lucadibello@proton.me>
2024-11-01 16:10:58 +0100

    Created script to run sonarqube scanner

---
+++ scripts/run_sonarqube.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Start Sonarqube console
+sonar-scanner \
+  -Dsonar.projectKey=resilience4j \
+  -Dsonar.sources=resilience4j/ \
+  -Dsonar.java.binaries=resilience4j/ \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=admin \
+  -Dsonar.password="Password&1" \
+  -Dsonar.scanner.skipJreProvisioning=true \
+  -Dsonar.exclusions=resilience4j/**/test/**,
